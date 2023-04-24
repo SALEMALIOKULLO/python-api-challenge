2065b2048aff54e6ba0526d4f3f70c281d35da15
salem okullo <127794394+SALEMALIOKULLO@users.noreply.github.com>
2023-04-24 18:59:24 -0500

    gitignore changes

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "Ya3d779ea205bdf81721c7fd864e6ead9"
+weather_api_key = " "
 
 # Geoapify API Key
-geoapify_key = "f9a866d29363484181051ad4189d7ead"
+geoapify_key = " "
